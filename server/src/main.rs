e8615167576ac066c72c485f90759a7fb953a158
Lucas Meurer <lucas.meurer@newcubator.com>
2022-08-11 13:34:06 +0200

    Create listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use futures_util::{StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let addr = "0.0.0.0:6789".to_string();
+
+    let listener = TcpListener::bind(addr).await?;
+
+    Ok(())
 }
+
