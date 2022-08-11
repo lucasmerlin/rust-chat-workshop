b641fd907f789af4e508f6a816ac67f67c7e9432
Lucas Meurer <lucas.meurer@newcubator.com>
2022-08-11 14:13:03 +0200

    Accept connections

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,5 @@
 use std::io::Error;
-use futures_util::{StreamExt};
+use futures_util::{TryStreamExt};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -8,6 +8,19 @@ async fn main() -> Result<(), Error> {
 
     let listener = TcpListener::bind(addr).await?;
 
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
+    }
+
     Ok(())
 }
 
+async fn accept_connection(stream: TcpStream) -> Result<(), tungstenite::Error> {
+    let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
+
+    if let Some(message) =  ws_stream.try_next().await? {
+        println!("{:?}", message);
+    }
+
+    Ok(())
+}
