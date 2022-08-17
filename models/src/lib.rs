40f6d457d84a46ec1ddd0d0b3c67201a06bc2a1e
Lucas Meurer <lucas.meurer@newcubator.com>
2022-08-17 13:15:39 +0200

    Make message clonable

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message {
     pub text: String,
     pub user: String,
