f4e0beac85c040b3e901461deb96b99032e65e49
Lucas Meurer <lucas.meurer@newcubator.com>
2022-09-07 09:35:09 +0200

    Deserialize Messages

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,9 @@
 use std::io::Error;
-use futures_util::{TryStreamExt};
+
+use futures_util::TryStreamExt;
 use tokio::net::{TcpListener, TcpStream};
+
+use models::ClientMessage;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -18,8 +21,10 @@ async fn main() -> Result<(), Error> {
 async fn accept_connection(stream: TcpStream) -> Result<(), tungstenite::Error> {
     let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
 
-    if let Some(message) =  ws_stream.try_next().await? {
-        println!("{:?}", message);
+    if let Some(tungstenite::Message::Text(text)) = ws_stream.try_next().await? {
+        if let Ok(ClientMessage::Connect { room, user }) = serde_json::from_str::<ClientMessage>(&text) {
+            println!("{user} is trying to join {room}");
+        }
     }
 
     Ok(())
