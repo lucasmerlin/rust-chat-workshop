29be51037c03703e0548f5047359a554eddf9ccb
Lucas Meurer <lucas.meurer@newcubator.com>
2022-09-07 11:58:46 +0200

    Impl room actor

--- server/src/room.rs
+++ server/src/room.rs
@@ -16,6 +16,23 @@ struct RoomActor {
     users: HashMap<u64, Connection>,
 }
 
+impl RoomActor {
+    pub async fn run(&mut self) {
+        while let Some(msg) = self.rx.recv().await {
+            match msg {
+                RoomMessage::Join(connection, send_uid) => {
+                    println!("User {} joined", connection.user);
+
+                    self.users.insert(self.next_connection_id, connection);
+                    send_uid.send(self.next_connection_id).unwrap();
+                    self.next_connection_id += 1;
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct RoomHandle {
     tx: mpsc::Sender<RoomMessage>,
