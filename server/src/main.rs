517a9a95d7dead1d398bea3c8962edf30cdfa6df
Lucas Meurer <lucas.meurer@newcubator.com>
2022-09-08 11:28:00 +0200

    Global room state II

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,12 +28,19 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream) -> Result<(), tungstenite::Error> {
+async fn accept_connection(stream: TcpStream, rooms: Rooms) -> Result<(), tungstenite::Error> {
     let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
 
     if let Some(tungstenite::Message::Text(text)) = ws_stream.try_next().await? {
         if let Ok(ClientMessage::Connect { room, user }) = serde_json::from_str::<ClientMessage>(&text) {
-            println!("{user} is trying to join {room}");
+            let handle = {
+                let mut rooms = rooms.lock().unwrap();
+                rooms
+                    .entry(room)
+                    .or_insert_with(|| RoomHandle::new()).clone()
+            };
+
+            handle.join(user, ws_stream).await;
         }
     }
 
